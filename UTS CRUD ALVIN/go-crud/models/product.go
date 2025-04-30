01955032b973edaec89f03177f95fc1609ef596d
alvinalfa12 <alvinalfarizi155@gmail.com>
2025-04-30 11:30:39 +0700

    Delete UTS CRUD ALVIN/go-crud directory

--- UTS CRUD ALVIN/go-crud/models/product.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Product struct {
-	ID         int       `json:"id"`
-	Name       string    `json:"name"`
-	Price      float64   `json:"price"`
-	CategoryID int       `json:"category_id"`
-	Category   *Category `json:"category,omitempty"`
-}
